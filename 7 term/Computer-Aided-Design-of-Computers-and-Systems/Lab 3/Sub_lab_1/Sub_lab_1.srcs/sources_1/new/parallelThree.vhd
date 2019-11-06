47c7dec546f9f0948fd9496e5eefe9d32281c0e3
NasterVill <skatrr@gmail.com>
2019-11-06 12:37:51 +0300

    CADoCaS lab 3 temp

---
+++ 7 term/Computer-Aided-Design-of-Computers-and-Systems/Lab 3/Sub_lab_1/Sub_lab_1.srcs/sources_1/new/parallelThree.vhd
@@ -0,0 +1,85 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/18/2019 12:57:04 PM
+-- Design Name: 
+-- Module Name: parallelThree - ParallelThree
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity parallelThree is
+    Port ( 
+        EI: in std_logic;
+        inputVec: in std_logic_vector(0 to 7);
+        EO, GS: out std_logic;
+        A: out std_logic_vector(2 downto 0)
+    );  
+end parallelThree;
+
+architecture ParallelThree of parallelThree is
+  signal tmpNotAndResult: std_logic;
+  
+begin
+  tmpNotAndResult <= not (
+    inputVec(0) and
+    inputVec(1) and
+    inputVec(2) and
+    inputVec(3) and
+    inputVec(4) and
+    inputVec(5) and
+    inputVec(6) and
+    inputVec(7) and
+    (not EI)
+  );
+
+  EO <= tmpNotAndResult;
+
+  -- GS <= (not (not EI)) or (not tmpNotAndResult);
+  GS <= not ((not EI) and tmpNotAndResult);
+
+  A(0) <= not (
+    ((not inputVec(1)) and inputVec(2) and inputVec(4) and inputVec(6) and (not EI)) or
+    ((not inputVec(3)) and inputVec(4) and inputVec(6) and (not EI)) or
+    ((not inputVec(5)) and inputVec(6) and (not EI)) or
+    ((not inputVec(7)) and (not EI))
+  );
+
+  A(1) <= not (
+    ((not inputVec(2)) and inputVec(4) and inputVec(5) and (not EI)) or
+    ((not inputVec(3)) and inputVec(4) and inputVec(5) and (not EI)) or
+    ((not inputVec(6)) and (not EI)) or
+    ((not inputVec(7)) and (not EI))
+  );
+
+  A(2) <= not (
+    ((not inputVec(4)) and (not EI)) or
+    ((not inputVec(5)) and (not EI)) or
+    ((not inputVec(6)) and (not EI)) or
+    ((not inputVec(7)) and (not EI))
+  );
+
+end ParallelThree;
---
+++ 7 term/Computer-Aided-Design-of-Computers-and-Systems/Lab 3/Sub_lab_1/Sub_lab_1.srcs/sources_1/new/sequence.vhd
@@ -0,0 +1,88 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/07/2019 02:30:01 PM
+-- Design Name: 
+-- Module Name: sequence - Sequence
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use ieee.std_logic_unsigned.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sequence is
+    Port ( 
+        EI: in std_logic;
+        inputVec: in std_logic_vector(0 to 7);
+        EO, GS: out std_logic;
+        A: out std_logic_vector(2 downto 0)
+    );
+end sequence;
+
+architecture Sequence of sequence is
+
+  begin
+    process (inputVec, EI) is
+    begin
+    
+      if (EI = '1') then
+        A <= "111";
+
+        GS <= '1';
+        EO <= '1';
+      else 
+        if (inputVec(7) = '0') then
+          A <= "000";
+        elsif (inputVec(6) = '0') then
+          A <= "001";
+        elsif (inputVec(5) = '0') then
+          A <= "010";
+        elsif (inputVec(4) = '0') then
+          A <= "011";  
+        elsif (inputVec(3) = '0') then
+          A <= "100";  
+        elsif (inputVec(2) = '0') then
+          A <= "101";  
+        elsif (inputVec(1) = '0') then
+          A <= "110";  
+        elsif (inputVec(0) = '0') then
+          A <= "111"; 
+        else
+          A <= "111";
+        end if;
+
+        if (inputVec = "11111111") then
+          GS <= '1';
+          EO <= '0';
+        else
+          GS <= '0';
+          EO <= '1';
+        end if;
+      end if;
+    
+    end process;
+    
+end Sequence;
