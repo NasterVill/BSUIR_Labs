b4bfa53b7e1428095e6d0fcd0e31ca9f8716af11
NasterVill <skatrr@gmail.com>
2019-11-13 00:02:45 +0300

    lab 4 msat refactoring

--- 7 term/Microprocessor-Systems-and-Tools/Lab 4/main.c
+++ 7 term/Microprocessor-Systems-and-Tools/Lab 4/main.c
@@ -1,22 +1,37 @@
 ﻿#include <msp430.h>
 #include <math.h>
 
-#define COLUMN_ADR_MSB              0x10  //Set SRAM col. addr. before write, last 4 bits = ca4-ca7
-#define COLUMN_ADR_LSB              0x00  //Set SRAM col. addr. before write, last 4 bits = ca0-ca3
-#define POW_CTL                     0x2F  //Set Power control - booster, regulator, and follower on
+
+typedef unsigned char uchar;
+
+
 #define SCROL_CTL                   0x40  //Scroll image up by SL rows (SL = last 5 bits), range:0-63
-#define PAGE_ADR                    0xB0  //Set SRAM page addr (pa = last 4 bits), range:0-8
+
+#define SET_MIRROR_SEG 				0xA0  //Normal mirror SEG (column) mapping (set bit0 to mirror display)
+
+#define SET_MIRROR_COM 				0xC0  //Normal mirror COM (row) mapping (set bit3 to mirror display)
+
+#define ALL_PIXEL_ON                0xA4  //Disable all pixel on (last bit 1 to turn on all pixels - does not affect memory)
+
+#define LCD_INVERSE                 0xA6  //Inverse display off (last bit 1 to invert display - does not affect memory)
+
+#define BIAS_RATIO_VCC              0xA2  //Set voltage bias ratio (BR = bit0)
+
+#define POW_CTL                     0x2F  //Set Power control - booster, regulator, and follower on
+
 #define SET_CONTRAST_RESISTOR       0x27  //Set internal resistor ratio Rb/Ra to adjust contrast
 #define MSB_ELECT_VOLUME            0x81  //Set Electronic Volume "PM" to adjust contrast
-#define LSB_ELECT_VOLUME            0x0F  //Set Electronic Volume "PM" to adjust contrast (PM = last 5 bits)
-#define ALL_PIXEL_ON                0xA4  //Disable all pixel on (last bit 1 to turn on all pixels - does not affect memory)
-#define LCD_INVERSE                 0xA6  //Inverse display off (last bit 1 to invert display - does not affect memory)
-#define LCD_EN                      0xAF  //Enable display (exit sleep mode & restore power)
-#define SET_MIRROR_SEG 				      0xA0  //Mirror SEG (column) mapping (set bit0 to mirror display)
-#define SET_MIRROR_COM 				      0xC0  //Mirror COM (row) mapping (set bit3 to mirror display)
-#define BIAS_RATIO_VCC              0xA2  //Set voltage bias ratio (BR = bit0)
+#define LSB_ELECT_VOLUME            0x10  //Set Electronic Volume "PM" to adjust contrast (PM = last 5 bits)
+
 #define ADV_CTL_MSB                 0xFA  //Set temp. compensation curve to -0.11%/C
 #define ADV_CTL_LSB                 0x90
+
+#define COLUMN_ADR_MSB              0x10  //Set SRAM col. addr. before write, last 4 bits = ca4-ca7
+#define COLUMN_ADR_LSB              0x00  //Set SRAM col. addr. before write, last 4 bits = ca0-ca3
+#define PAGE_ADR                    0xB0  //Set SRAM page addr (pa = last 4 bits), range:0-8
+
+#define LCD_EN                      0xAF  //Enable display (exit sleep mode & restore power)
+
 #define ROWS 9
 #define COLUMNS 6
 #define PAGES 2
@@ -24,7 +39,6 @@
 #define COLUMN_OFFSET_BIG 31
 #define COLUMN_OFFSET_NONE 0
 
-typedef unsigned char uchar;
 
 void Delay(int value);
 int GetS1State();
@@ -40,6 +54,7 @@ void Dogs102x6_writeCommand(uchar *sCmd, uchar i);
 
 void Clear(void);
 void ShowNumber(void);
+
 
 uchar mirror_modes[2][1] = {{0xA0}, {0xA1}};
 
@@ -67,24 +82,25 @@ int column_offset = COLUMN_OFFSET_BIG; // 0 - default is COLUMN_OFFSET_BIG, 1 - 
 
 uchar current_page = 0, current_column = 0;
 
-uchar Dogs102x6_initMacro[] = {
-  SCROL_CTL,
-  SET_MIRROR_SEG,
-  SET_MIRROR_COM,
-  ALL_PIXEL_ON,
-  LCD_INVERSE,
-  BIAS_RATIO_VCC,
-  POW_CTL,
-  SET_CONTRAST_RESISTOR,
-  MSB_ELECT_VOLUME,
-  LSB_ELECT_VOLUME,
-  ADV_CTL_MSB,
-  ADV_CTL_LSB,
-  LCD_EN,
-  PAGE_ADR,
-  COLUMN_ADR_MSB,
-  COLUMN_ADR_LSB
+uchar LCD_INIT_COMMANDS_PART_1[7] = {
+	SCROL_CTL,
+	SET_MIRROR_SEG,
+	SET_MIRROR_COM,
+	ALL_PIXEL_ON,
+	LCD_INVERSE,
+	BIAS_RATIO_VCC,
+	POW_CTL
 };
+uchar LCD_INIT_COMMANDS_PART_2[6] = {
+	SET_CONTRAST_RESISTOR,
+	MSB_ELECT_VOLUME,
+	LSB_ELECT_VOLUME,
+	ADV_CTL_MSB,
+	ADV_CTL_LSB,
+	LCD_EN,
+};
+int AMOUNT_OF_COMMANDS_1 = 7;
+int AMOUNT_OF_COMMANDS_2 = 6;
 
 int main(void)
 {
@@ -105,194 +121,211 @@ int main(void)
 
 void SetupSPI()
 {
-  UCB1CTL1 |= UCSWRST;
-  UCB1CTL0 = (UCCKPH | UCMSB | UCMST | UCSYNC | UCMODE_0);
-  UCB1CTL1 = UCSSEL_2 | UCSWRST;
-  UCB1BR0 = 0x02;
-  UCB1BR1 = 0;
+	UCB1CTL1 |= UCSWRST;	// set UCSWRST bit to disabel USCI and change its control registeres
 
-  UCB1CTL1 &= ~UCSWRST;
-  UCB1IFG &= ~UCRXIFG;
-  Dogs102x6_writeCommand(Dogs102x6_initMacro, 13);
-  P7OUT |= BIT4;
+	UCB1CTL0 = (
+		UCCKPH 	&	// UCCKPH - 1: change out on second signal change, capture input on first one)
+		~UCCKPL |	// UCCKPL - 0: active level is 1
+		UCMSB 	|	// MSB comes first, LSB is next
+		UCMST 	|	// Master mode
+		UCSYNC 	|	// Synchronious mode
+		UCMODE_0	// 3 pin SPI mode
+	);
+
+	// set SMCLK as source and keep RESET
+	UCB1CTL1 = UCSSEL_2 | UCSWRST;
+
+	// set frequency divider
+	UCB1BR0 = 0x01;	// LSB to 1
+	UCB1BR1 = 0;	// MSB to 0
+
+	UCB1CTL1 &= ~UCSWRST;	// enable USCI
+	UCB1IFG &= ~UCRXIFG;	// reset int flag (which is set after input shift register gets data)
+	Dogs102x6_writeCommand(LCD_INIT_COMMANDS_PART_1, AMOUNT_OF_COMMANDS_1);
+
+	// about 120 ms with MCLK set to 1MHz
+	Delay(125000);
+
+	Dogs102x6_writeCommand(LCD_INIT_COMMANDS_PART_2, AMOUNT_OF_COMMANDS_2);
 }
 
 void SetupLCD()
 {
-  P5DIR |= BIT7;
-  P5OUT &= BIT7;
-  P5OUT |= BIT7;
-  P7DIR |= BIT4;
-  P7OUT &= ~BIT4;
-  P5DIR |= BIT6;
-  P5OUT &= ~BIT6;
+	P5DIR |= BIT7;	// port init for LCD operations
+	P5OUT &= BIT7;	// set RST (active low)
+	P5OUT |= BIT7;	// reset RST (inactive is high)
+	P7DIR |= BIT4;	// select LCD for chip
+	P7OUT &= ~BIT4;	// CS is active low
+	P5DIR |= BIT6;	// Command/Data for LCD
+	P5OUT &= ~BIT6;	// CD low for command
 
-  P4SEL |= BIT1 | BIT3;
-  P4DIR |= BIT1 | BIT3;
+	// P4.1 -- SIMO, P4.3 -- select PM_UCB1CLK
+	P4SEL |= BIT1 | BIT3;
+	P4DIR |= BIT1 | BIT3;
 
-  P7DIR |= BIT6;
-  P7OUT |= BIT6;
-  P7SEL &= ~BIT6;
+	P7DIR |= BIT6;	// init
+	P7OUT |= BIT6;	// backlit
+	P7SEL &= ~BIT6; // USE PWM to controll brightness
+
+
+	P7OUT |= BIT4;	// CS = 1 (Deselect LCD) (stop setting it up)
 }
 
 void SetupButtons()
 {
-  P1DIR &= ~BIT7;
-  P2DIR &= ~BIT2;
+	P1DIR &= ~BIT7;
+	P2DIR &= ~BIT2;
 
-  P1REN |= BIT7;
-  P2REN |= BIT2;
+	P1REN |= BIT7;
+	P2REN |= BIT2;
 
-  P1OUT |= BIT7;
-  P2OUT |= BIT2;
+	P1OUT |= BIT7;
+	P2OUT |= BIT2;
 
-  P1IE |= BIT7;
-  P1IES |= BIT7;
-  P1IFG = 0;
+	P1IE |= BIT7;
+	P1IES |= BIT7;
+	P1IFG = 0;
 
-  P2IE |= BIT2;
-  P2IES |= BIT2;
-  P2IFG = 0;
+	P2IE |= BIT2;
+	P2IES |= BIT2;
+	P2IFG = 0;
 }
 
 void __SPI_SetAddress(uchar page, uchar column)
 {
-  uchar cmd[1];
+	uchar cmd[1];
 
-  if (page > 7)
-  {
-    page = 7;
-  }
+	if (page > 7)
+	{
+	page = 7;
+	}
 
-  if (column > 101)
-  {
-    column = 101;
-  }
+	if (column > 101)
+	{
+	column = 101;
+	}
 
-  cmd[0] = PAGE_ADR + (7 - page);
-  uchar H = 0x00;
-  uchar L = 0x00;
-  uchar column_address[] = { COLUMN_ADR_MSB, COLUMN_ADR_LSB };
+	cmd[0] = PAGE_ADR + (7 - page);
+	uchar H = 0x00;
+	uchar L = 0x00;
+	uchar column_address[] = { COLUMN_ADR_MSB, COLUMN_ADR_LSB };
 
-  current_page = page;
-  current_column = column;
+	current_page = page;
+	current_column = column;
 
-  L = (column & 0x0F);
-  H = (column & 0xF0);
-  H = (H >> 4);
+	L = (column & 0x0F);
+	H = (column & 0xF0);
+	H = (H >> 4);
 
-  column_address[0] = COLUMN_ADR_LSB + L;
-  column_address[1] = COLUMN_ADR_MSB + H;
+	column_address[0] = COLUMN_ADR_LSB + L;
+	column_address[1] = COLUMN_ADR_MSB + H;
 
-
-  Dogs102x6_writeCommand(cmd, 1);
-  Dogs102x6_writeCommand(column_address, 2);
+	Dogs102x6_writeCommand(cmd, 1);
+	Dogs102x6_writeCommand(column_address, 2);
 }
 
 void Dogs102x6_writeCommand(uchar *sCmd, uchar i)
 {
-  // CS Low
-  P7OUT &= ~BIT4;
+	// CS Low
+	P7OUT &= ~BIT4;
 
-  // CD Low
-  P5OUT &= ~BIT6;
-  while (i)
-  {
-    // USCI_B1 TX buffer ready?
-    while (!(UCB1IFG & UCTXIFG)) ;
+	// CD Low
+	P5OUT &= ~BIT6;
+	while (i)
+	{
+		// USCI_B1 TX buffer ready?
+		while (!(UCB1IFG & UCTXIFG)) ;
 
-    // Transmit data
-    UCB1TXBUF = *sCmd;
+		// Transmit data
+		UCB1TXBUF = *sCmd;
 
-    // Increment the pointer on the array
-    sCmd++;
+		// Increment the pointer on the array
+		sCmd++;
 
-    // Decrement the Byte counter
-    i--;
-  }
+		// Decrement the Byte counter
+		i--;
+	}
 
-  // Wait for all TX/RX to finish
-  while (UCB1STAT & UCBUSY);
+	// Wait for all TX/RX to finish
+	while (UCB1STAT & UCBUSY);
 
-  // Dummy read to empty RX buffer and Clear any overrun conditions
-  UCB1RXBUF;
+	// Dummy read to empty RX buffer and Clear any overrun conditions
+	UCB1RXBUF;
 
-  // CS High
-  P7OUT |= BIT4;
+	// CS High
+	P7OUT |= BIT4;
 }
 
 void Dogs102x6_writeData(uchar *sData, uchar i)
 {
-  // CS Low
-  P7OUT &= ~BIT4;
-  //CD High
-  P5OUT |= BIT6;
+	// CS Low
+	P7OUT &= ~BIT4;
+	//CD High
+	P5OUT |= BIT6;
 
-  while (i)
-  {
-    current_column++;
+	while (i)
+	{
+		current_column++;
 
-    if (current_column > 101)
-    {
-      current_column = 101;
-    }
+		if (current_column > 101)
+		{
+			current_column = 101;
+		}
 
-    // USCI_B1 TX buffer ready?
-    while (!(UCB1IFG & UCTXIFG));
+		// USCI_B1 TX buffer ready?
+		while (!(UCB1IFG & UCTXIFG));
 
-    // Transmit data and increment pointer
-    UCB1TXBUF = *sData++;
+		// Transmit data and increment pointer
+		UCB1TXBUF = *sData++;
 
-    // Decrement the Byte counter
-    i--;
-  }
+		// Decrement the Byte counter
+		i--;
+	}
 
-  // Wait for all TX/RX to finish
-  while (UCB1STAT & UCBUSY);
+	// Wait for all TX/RX to finish
+	while (UCB1STAT & UCBUSY);
 
-  // Dummy read to empty RX buffer and Clear any overrun conditions
-  UCB1RXBUF;
+	// Dummy read to empty RX buffer and Clear any overrun conditions
+	UCB1RXBUF;
 
-  // CS High
-  P7OUT |= BIT4;
-
-  // Restore original GIE state
+	// CS High
+	P7OUT |= BIT4;
 }
 
 void ShowNumber(void)
 {
-  volatile int length = 1;
-  volatile int digit = 0;
-  volatile int j = 0;
-  volatile int i = 10;
+	volatile int length = 1;
+	volatile int digit = 0;
+	volatile int j = 0;
+	volatile int i = 10;
 
-  while(1)
-  {
-    if(number / i != 0)
-    {
-      i *= 10;
-      length++;
-    }
-    else
-    {
-      break;
-    }
-  }
-
-  int temp = number;
-  for(j = 0; j < length; j++)
-  {
-    digit = (int)(temp % 10);
-
-    digit = digit < 0 ? (-1) * digit : digit;
-
-	if (digit < 10)
+	while(1)
 	{
-		__SPI_SetAddress(0, column_offset + j * COLUMNS);
-		Dogs102x6_writeData(digits[digit][0], COLUMNS);
-		__SPI_SetAddress(1, column_offset + j * COLUMNS);
-		Dogs102x6_writeData(digits[digit][1], COLUMNS);
+		if(number / i != 0)
+		{
+			i *= 10;
+			length++;
+		}
+		else
+		{
+			break;
+		}
 	}
+
+	int temp = number;
+	for(j = 0; j < length; j++)
+	{
+		digit = (int)(temp % 10);
+
+		digit = digit < 0 ? (-1) * digit : digit;
+
+		if (digit < 10)
+			{
+			__SPI_SetAddress(0, column_offset + j * COLUMNS);
+			Dogs102x6_writeData(digits[digit][0], COLUMNS);
+			__SPI_SetAddress(1, column_offset + j * COLUMNS);
+			Dogs102x6_writeData(digits[digit][1], COLUMNS);
+		}
 
 		temp /= 10;
 	}
@@ -315,78 +348,78 @@ void ShowNumber(void)
 
 void Clear(void)
 {
-  uchar lcd_data[] = {0x00};
-  uchar page, column;
+	uchar lcd_data[] = {0x00};
+	uchar page, column;
 
-  for (page = 0; page < 8; page++)
-  {
-    __SPI_SetAddress(page, 0);
-    for (column = 0; column < 132; column++)
-    {
-      Dogs102x6_writeData(lcd_data, 1);
-    }
-  }
+	for (page = 0; page < 8; page++)
+	{
+		__SPI_SetAddress(page, 0);
+		for (column = 0; column < 132; column++)
+		{
+			Dogs102x6_writeData(lcd_data, 1);
+		}
+	}
 }
 
 #pragma vector = PORT1_VECTOR
 __interrupt void __S1_ButtonHandler(void)
 {
-  Delay(DELAY);
+	Delay(DELAY);
 
-  if(GetS1State())
-  {
-    Clear();
-    number = number - subtrahend;
-    ShowNumber();
-  }
+	if(GetS1State())
+	{
+		Clear();
+		number = number - subtrahend;
+		ShowNumber();
+	}
 
-  P1IFG &= ~BIT7;
+	P1IFG &= ~BIT7;
 }
 
 
 #pragma vector = PORT2_VECTOR
 __interrupt void __S2_ButtonHandler(void)
 {
-  Delay(DELAY);
+	Delay(DELAY);
 
-  if(GetS2State())
-  {
-    Clear();
+	if(GetS2State())
+	{
+		Clear();
 
-    if (mirror_mode == 0)
-    {
-      column_offset = COLUMN_OFFSET_NONE;
-      mirror_mode = 1;
-    }
-    else
-    {
-      column_offset = COLUMN_OFFSET_BIG;
-      mirror_mode = 0;
-    }
+		if (mirror_mode == 0)
+		{
+			column_offset = COLUMN_OFFSET_NONE;
+			mirror_mode = 1;
+		}
+		else
+		{
+			column_offset = COLUMN_OFFSET_BIG;
+			mirror_mode = 0;
+		}
 
-    Dogs102x6_writeCommand(mirror_modes[mirror_mode], 1);
-    ShowNumber();
-  }
+		Dogs102x6_writeCommand(mirror_modes[mirror_mode], 1);
+		ShowNumber();
+	}
 
-  P2IFG &= ~BIT2;
+	P2IFG &= ~BIT2;
 }
 
 void Delay(int value)
 {
-  volatile long int i = 0;
-  volatile long int temp = 0;
-  for (; i < value; i++)
-  {
-    temp++;
-  }
+	volatile long int i = 0;
+	volatile long int temp = 0;
+	for (; i < value; i++)
+	{
+		temp++;
+	}
 }
 
 int GetS1State()
 {
-  return (P1IN & BIT7) ? 0 : 1;
+	return (P1IN & BIT7) ? 0 : 1;
 }
 
 int GetS2State()
 {
-  return (P2IN & BIT2) ? 0 : 1;
+	return (P2IN & BIT2) ? 0 : 1;
 }
